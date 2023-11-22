c288eab07bfd0498ade99d723fe0864cfc0ab0a0
Dai <kobayashi.daigo3@gmail.com>
2023-11-22 20:28:08 +0900

    feat: フォームから送信した値をaddEventListenerで取得できるように実装

--- kiyoto-jspractice/index.js
+++ kiyoto-jspractice/index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", function (event){
+  event.preventDefault();
+  console.log(input.value)
+})
